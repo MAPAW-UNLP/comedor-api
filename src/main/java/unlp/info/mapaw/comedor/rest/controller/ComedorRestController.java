139be93d662b0bb61401774874055eee56f487db
Facundo Iaccarino <fiaccarino@mecon.gov.ar>
2020-09-29 13:51:32 -0300

    Home permitAll

--- src/main/java/unlp/info/mapaw/comedor/rest/controller/ComedorRestController.java
+++ src/main/java/unlp/info/mapaw/comedor/rest/controller/ComedorRestController.java
@@ -1,13 +1,12 @@
 package unlp.info.mapaw.comedor.rest.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api")
 public class ComedorRestController {
 	
 	@GetMapping("/home")
@@ -15,7 +14,6 @@ public class ComedorRestController {
 		return "Welcome home!";
 	}
 	
-	@PreAuthorize("isAuthenticated()")  
 	@GetMapping("/restricted")  
 	@ResponseBody  
 	public String restricted() {  
