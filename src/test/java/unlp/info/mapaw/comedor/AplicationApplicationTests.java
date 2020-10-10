cfba7bd843f2f9ba723ff3faab15aa8f67555523
Facundo Iaccarino <facundo.iaccarino@gmail.com>
2020-10-09 22:22:56 -0300

    se comentan applicationtest

--- src/test/java/unlp/info/mapaw/comedor/AplicationApplicationTests.java
+++ src/test/java/unlp/info/mapaw/comedor/AplicationApplicationTests.java
@@ -1,13 +1,13 @@
-package unlp.info.mapaw.comedor;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AplicationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package unlp.info.mapaw.comedor;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AplicationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
