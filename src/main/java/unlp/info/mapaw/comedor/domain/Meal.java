683ba609e21c3aa6ef430488cf82fa40d6eaa817
Facundo Iaccarino <facundo.iaccarino@gmail.com>
2020-10-15 20:37:47 -0300

    change mappin boolean

--- src/main/java/unlp/info/mapaw/comedor/domain/Meal.java
+++ src/main/java/unlp/info/mapaw/comedor/domain/Meal.java
@@ -22,10 +22,10 @@ public class Meal extends AbstractEntity {
 	@JoinColumn(name = "id_meal")
 	private List<Dish> items= new ArrayList<>();
 	
-	@Column(name="suitable_for_vegetarians")
+	@Column(name="suitable_for_vegetarians", columnDefinition="TINYINT", length=1)
 	private boolean suitableForVegetarians;
 	
-	@Column(name="suitable_for_celiacs")
+	@Column(name="suitable_for_celiacs", columnDefinition="TINYINT", length=1)
 	private boolean suitableForCeliacs;
 	
 	private String observations;
