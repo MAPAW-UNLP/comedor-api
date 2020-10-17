b154e83400de9db490f125657c3761c0df738e5d
Facundo Iaccarino <facundo.iaccarino@gmail.com>
2020-10-17 19:52:27 -0300

    add access denied message

--- src/main/java/unlp/info/mapaw/comedor/rest/error/CustomExceptionHandler.java
+++ src/main/java/unlp/info/mapaw/comedor/rest/error/CustomExceptionHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,6 +19,14 @@ public class CustomExceptionHandler {
 			final HttpServletRequest request) {
 		ErrorDTO dto = new ErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
 				HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), rse.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(dto.getStatus()).contentType(MediaType.APPLICATION_JSON).body(dto);
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ErrorDTO> handleServiceException(final AccessDeniedException rse,
+			final HttpServletRequest request) {
+		ErrorDTO dto = new ErrorDTO(HttpStatus.FORBIDDEN.value(),
+				"Access denied", null, request.getRequestURI());
 		return ResponseEntity.status(dto.getStatus()).contentType(MediaType.APPLICATION_JSON).body(dto);
 	}
 }
