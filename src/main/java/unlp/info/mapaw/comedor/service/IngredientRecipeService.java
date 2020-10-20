52e6c5e1fe1e0372a4e99452558d6dd52ac717cc
Facundo Iaccarino <facundo.iaccarino@gmail.com>
2020-10-20 20:04:25 -0300

    more validates

--- src/main/java/unlp/info/mapaw/comedor/service/IngredientRecipeService.java
+++ src/main/java/unlp/info/mapaw/comedor/service/IngredientRecipeService.java
@@ -3,7 +3,9 @@ package unlp.info.mapaw.comedor.service;
 import org.springframework.stereotype.Service;
 
 import unlp.info.mapaw.comedor.domain.IngredientRecipe;
+import unlp.info.mapaw.comedor.dto.DishRecipeDTO;
 import unlp.info.mapaw.comedor.dto.IngredientRecipeDTO;
+import unlp.info.mapaw.comedor.exception.ServiceException;
 
 @Service
 public class IngredientRecipeService extends AbstractEntityService<IngredientRecipeDTO, IngredientRecipe> {
@@ -30,6 +32,14 @@ public class IngredientRecipeService extends AbstractEntityService<IngredientRec
 	@Override
 	protected IngredientRecipe createEmptyEntity() {
 		return new IngredientRecipe();
+	}
+	
+	@Override
+	protected void validateDTO(IngredientRecipeDTO dto) {
+		if (dto.getName() == null)
+			throw new ServiceException("Name is required");
+		if (dto.getMeasurement() == null)
+			throw new ServiceException("Measurement is required");
 	}
 
 }
